c363b3caab7c615ce944409b5abd5da319333126
Vinoryz <brofistjr10@gmail.com>
2025-04-06 20:34:05 +0700

    a

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,6 @@ RUN pip install -r requirements.txt
 
 COPY . .
 
-EXPOSE 5000
+EXPOSE 9999
 
 CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -19,6 +19,6 @@ def health():
     return jsonify(response)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 9999))
     app.run(host='0.0.0.0', port=port)
 
