554bf7460a39b63c867412d9ab264f08f4e012c5
Vinoryz <brofistjr10@gmail.com>
2025-04-06 20:39:41 +0700

    a

--- app.py
+++ app.py
@@ -1,24 +1,20 @@
-from flask import Flask, jsonify
+from flask import Flask
 import os
 import time
-import datetime
 
 app = Flask(__name__)
 start_time = time.time()
 
-@app.route("/health")
+@app.route('/health')
 def health():
-    uptime = time.time() - start_time
-    response = {
+    return {
         "nama": "Tom Marvolo Riddle",
         "nrp": "5025239999",
         "status": "UP",
-        "timestamp": datetime.datetime.utcnow().isoformat() + "Z",
-        "uptime": f"{uptime:.2f} seconds"
+        "timestamp": time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()),
+        "uptime": f"{int(time.time() - start_time)} seconds"
     }
-    return jsonify(response)
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 9999))
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
-
